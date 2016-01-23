286442fd9b7723cc29aeb050e1360d3a1f261d7b
Koji Ota <afterjnih@gmail.com>
2016-01-23 13:47:38 +0900

    Initial Commit

---
+++ sniffer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sniffer
+//
+//  Created by Koji Ota on 2016/01/23.
+//  Copyright © 2016年 Koji Ota. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
